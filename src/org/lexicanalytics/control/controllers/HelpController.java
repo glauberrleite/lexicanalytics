991969ab87e5f0e3d7b70b3bf8e8e65f7a20d7a0
glauberrleite <leite.glauber.rodrigues@gmail.com>
2016-08-07 11:13:49 -0300

    Browser for help links
    Removed unused classes

--- src/org/lexicanalytics/control/controllers/HelpController.java
+++ src/org/lexicanalytics/control/controllers/HelpController.java
@@ -1,8 +1,12 @@
 package org.lexicanalytics.control.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 import org.lexicanalytics.application.Main;
 import org.lexicanalytics.model.BaseController;
@@ -37,6 +41,23 @@ public class HelpController extends BaseController {
 		WebView browser = new WebView();
 		WebEngine webEngine = browser.getEngine();
 		webEngine.load(link);
+
+		VBox root = new VBox();
+		root.getChildren().add(browser);
+		
+		Scene scene = new Scene(root, 800, 500);
+		
+		
+		Stage stage = new Stage();
+		stage.setTitle("Lexicanalytics Browser");
+		stage.getIcons().add(
+				new Image(getClass().getClassLoader().getResourceAsStream(
+						"org/lexicanalytics/resources/logo.png")));
+		stage.setScene(scene);
+		stage.show();
+		
+		
+		
 		
 	}
 }
